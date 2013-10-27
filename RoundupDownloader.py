1a095f89285eec7e2d4c080912de4b8935cd7a43
Mark Bidewell <mbidewel@gmail.com>
2013-10-27 12:53:05 -0400

    Begin adding version tracking.

--- RoundupDownloader.py
+++ RoundupDownloader.py
@@ -1,6 +1,24 @@
 from lxml import etree
+from lxml.cssselect import CSSSelector
 
 BASE_URL="http://ivyroundup.googlecode.com/svn/trunk/repo"
+
+class IvyModule:
+    @staticmethod
+    def get_module(moduledef):
+        mod = IvyModule(moduledef[0].text)
+        for sp in moduledef[2].xpath("//*/span"):
+            mod.add_version(sp.text, sp.getparent().get("href"))
+
+    def __init__(self, name):
+        self.name = name;
+        self.versions = {}
+
+    def add_version(self, version, url):
+        self.versions[version] = url
+
+    def get_name(self):
+        return self.name
 
 
 def main():
@@ -16,12 +34,12 @@ def main():
         tdlist = list(row)
     
         if not tdlist[1].text in ivy:
-            ivy[tdlist[1].text] = [tdlist[0].text]
+            ivy[tdlist[1].text] = [IvyModule.get_module(tdlist)]
         else:
-            ivy[tdlist[1].text].append(tdlist[0].text)
+            ivy[tdlist[1].text].append(IvyModule.get_module(tdlist))
 
         for c in ivy.keys():
-            print "%s => %s" % (c, ','.join(ivy[c]))
+            print "%s => %s" % (c, ivy[c][0].get_name())
 
 if __name__ == "__main__":
     main()
