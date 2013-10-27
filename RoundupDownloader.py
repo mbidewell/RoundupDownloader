8fbdd2b6d8ca6cefcb3ac523c0939cc5b0080f3d
Mark Bidewell <mbidewell@localhost.localdomain>
2013-10-27 15:31:48 -0400

    begin cli

--- RoundupDownloader.py
+++ RoundupDownloader.py
@@ -1,5 +1,7 @@
 from lxml import etree
-from lxml.cssselect import CSSSelector
+
+import argparse
+
 
 BASE_URL="http://ivyroundup.googlecode.com/svn/trunk/repo"
 
@@ -7,8 +9,10 @@ class IvyModule:
     @staticmethod
     def get_module(moduledef):
         mod = IvyModule(moduledef[0].text)
-        for sp in moduledef[2].xpath("//*/span"):
+        for sp in moduledef[2].xpath("./*/span"):
             mod.add_version(sp.text, sp.getparent().get("href"))
+            
+	return mod
 
     def __init__(self, name):
         self.name = name;
@@ -19,9 +23,12 @@ class IvyModule:
 
     def get_name(self):
         return self.name
+      
+    def get_versions(self):
+        return self.versions.keys()
 
 
-def main():
+def initialize_ivyloader():
     xslt = etree.XSLT(etree.parse(BASE_URL+"/xsl/modules.xsl"))
     xml = etree.parse(BASE_URL+"/modules.xml")
     html = xslt(xml)
@@ -38,8 +45,17 @@ def main():
         else:
             ivy[tdlist[1].text].append(IvyModule.get_module(tdlist))
 
-        for c in ivy.keys():
-            print "%s => %s" % (c, ivy[c][0].get_name())
+
+def main():
+    parser = argparse.ArgumentParser(description='Search and download from IvyRoundup')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--find", "-f", nargs='+',
+			help="Search for ivy component.  At least one of org, module, version required" )
+    group.add_argument("--resolve", "-r", nargs=3,  help="Resolve ivy component")
+    group.add_argument("--install", "-i", nargs=3, help="Install ivy component")
+    group.add_argument("--list", "-l", help="List all ivy components")
+    args = parser.parse_args()
+    print(args)
 
 if __name__ == "__main__":
     main()
